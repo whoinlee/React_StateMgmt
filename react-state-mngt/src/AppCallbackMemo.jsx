deba6c98b6b992e10b3728b4ebdab9830d3876ca
WhoIN Lee <whoinlee@gmail.com>
2023-09-24 15:06:05 -0400

    added useCallback

--- react-state-mngt/src/AppCallbackMemo.jsx
+++ react-state-mngt/src/AppCallbackMemo.jsx
@@ -1,26 +1,28 @@
 /* eslint-disable react/prop-types */
-import { useState, useMemo } from 'react'
+import { useState, useMemo, useCallback } from 'react'
 import './App.css'
 
-function SortedList ( { label, list } ) {
+function SortedList ( { label, list, sortFunc } ) {
   const sortedList = useMemo(() => {
-    return [...list].sort();
-  }, [list]);
-  return (
-    // <div>Sorted {label}: {sortedList.join(", ")}</div>
-    <div>Sorted {label}: {sortedList.join(", ")}</div>
-  )
+    console.log("sortedList")
+    // return [...list].sort();
+    return [...list].sort(sortFunc);
+  }, [list, sortFunc]);
+
+  return <div>Sorted {label}: {sortedList.join(", ")}</div>
 }
 
 const AppCallbackMemo = () => {
   const [names] = useState(["name3", "name1", "name2", "nameN"]);
   // const sortedNames = useMemo(() => [...names].sort(), [names]);
 
+  const sortFunc = useCallback((a, b) => a.localeCompare(b) * -1, []);
+
   return (
     <>  
       <div>Names: {names.join(", ")}</div>
       {/* <div>Sorted Names: {sortedNames.join(", ")}</div> */}
-      <SortedList label="Names" list={names} />
+      <SortedList label="Names" list={names} sortFunc={sortFunc} />
     </>
   )
 }
