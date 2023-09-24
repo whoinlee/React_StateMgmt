6df6e4bd544fe1c48b2b578dfb422bc13c3c97fa
WhoIN Lee <whoinlee@gmail.com>
2023-09-24 14:53:58 -0400

    SortedList as a separate component

--- react-state-mngt/src/AppCallbackMemo.jsx
+++ react-state-mngt/src/AppCallbackMemo.jsx
@@ -1,16 +1,26 @@
+/* eslint-disable react/prop-types */
 import { useState, useMemo } from 'react'
-
 import './App.css'
 
+function SortedList ( { label, list } ) {
+  const sortedList = useMemo(() => {
+    return [...list].sort();
+  }, [list]);
+  return (
+    // <div>Sorted {label}: {sortedList.join(", ")}</div>
+    <div>Sorted {label}: {sortedList.join(", ")}</div>
+  )
+}
 
 const AppCallbackMemo = () => {
   const [names] = useState(["name3", "name1", "name2", "nameN"]);
-  const sortedNames = useMemo(() => [...names].sort(), [names]);
+  // const sortedNames = useMemo(() => [...names].sort(), [names]);
 
   return (
     <>  
       <div>Names: {names.join(", ")}</div>
-      <div>Sorted Names: {sortedNames.join(", ")}</div>
+      {/* <div>Sorted Names: {sortedNames.join(", ")}</div> */}
+      <SortedList label="Names" list={names} />
     </>
   )
 }
