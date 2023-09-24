4aab7bc8dea62817866006cd77c0073e2f65950c
WhoIN Lee <whoinlee@gmail.com>
2023-09-24 12:20:43 -0400

    useReducer way 1st commit

---
+++ react-state-mngt/src/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import AppUseReducer from './App.jsx';
+// import App from './App.jsx';
+// import App from './App.jsx';
+// import App from './App.jsx';
+// import App from './App.jsx';
+// import App from './App.jsx';
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AppUseReducer />
+  </React.StrictMode>,
+)
